ef78e5850c403f74c41c570af070ff6998e9c8d5
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-07 09:33:49 -0700

    Finish tests for patron and get patron class setup

---
+++ lib/patron.rb
@@ -0,0 +1,15 @@
+class Patron
+  attr_accessor :interests
+
+  attr_reader :name,
+              :spending_money
+  def initialize(name, spending_money)
+    @name = name
+    @spending_money = spending_money
+    @interests = []
+  end
+
+  def add_interests(interest)
+    @interests << interest
+  end  
+end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -4,10 +4,24 @@ require './lib/patron'
 
 class PatronTest < Minitest::Test
   def test_it_exists_and_has_attributes
-    patron = Patron.new("Bob", 20)
+    patron_1 = Patron.new("Bob", 20)
 
-    assert_instance_of Patron, patron
-    assert_equal "Bob", patron.name
-    assert_equal 20, patron.spending_money
+    assert_instance_of Patron, patron_1
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
   end
+
+  def test_patron_has_empty_interests
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal patron_1.interests, []
+  end
+
+  def test_patron_can_add_interests
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interests("Dead Sea Scrolls")
+    patron_1.add_interests("Gems and Minerals")
+
+    assert_equal patron_1.interests, ["Dead Sea Scrolls", "Gems and Minerals"]
+  end  
 end
