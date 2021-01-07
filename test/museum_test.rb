3fb1b0fe672de881b1fe5365574a4df6b6b4f537
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-07 10:14:09 -0700

    Finish Iteration 2

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -29,5 +29,23 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(imax)
 
     assert_equal dmns.exhibits, [gems_and_minerals, dead_sea_scrolls, imax]
+  end
 
+  def test_museum_can_recommend_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interests("Dead Sea Scrolls")
+    patron_1.add_interests("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interests("IMAX")
+
+    assert_equal dmns.recommend_exhibits(patron_1), [gems_and_minerals, dead_sea_scrolls]
+    assert_equal dmns.recommend_exhibits(patron_2), [imax]
+  end
 end
