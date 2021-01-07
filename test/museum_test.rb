5d877dfcc4af16bb208d5f3d19aaf3dfd2dc818e
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-07 09:44:13 -0700

    Add exhibit test to museum test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,7 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
 
-end  
+  def test_it_exists_and_has_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+    assert_equal dmns.name, "Denver Museum of Nature and Science"
+    assert_equal dmns.exhibits, []
+
+end
