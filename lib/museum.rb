94c6268e08078755488eddc4f9262be965c5c311
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-07 09:45:33 -0700

    Add exhibit test to museum test

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,2 +1,16 @@
 require './lib/patron'
 require './lib/exhibit'
+
+class Museum
+  attr_accessor :exhibits
+
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -11,6 +11,23 @@ class MuseumTest < Minitest::Test
 
     assert_instance_of Museum, dmns
     assert_equal dmns.name, "Denver Museum of Nature and Science"
+  end
+
+  def test_museum_has_no_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
     assert_equal dmns.exhibits, []
+  end
+
+  def test_museum_can_add_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal dmns.exhibits, [gems_and_minerals, dead_sea_scrolls, imax]
 
 end
