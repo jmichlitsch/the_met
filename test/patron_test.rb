4b345b3308afd97806b9db6cb3a7cba5eeecc377
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-07 09:23:54 -0700

    Add tests for patron_test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -3,5 +3,11 @@ require 'minitest/pride'
 require './lib/patron'
 
 class PatronTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    patron = Patron.new("Bob", 20)
 
-end  
+    assert_instance_of Patron, patron
+    assert_equal "Bob", patron.name
+    assert_equal 20, patron.spending_money
+  end
+end
